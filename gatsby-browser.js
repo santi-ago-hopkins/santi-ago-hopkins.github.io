bfdde81d4a63c3cc5ebd77de88107ba23c921256
santiagohopkins <santiagoahopkins@gmail.com>
2024-09-15 15:18:42 -0700

    small fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,7 +3,7 @@ import "@fontsource/montserrat/400.css"; // Regular
 import "@fontsource/montserrat/700.css"; // Bold
 import '/src/fonts/fonts.css'
 import React from 'react';
-import App from './src/App';
+import App from './src/app.js';
 
 export const wrapPageElement = ({ element, props }) => {
   return <App {...props}>{element}</App>;
