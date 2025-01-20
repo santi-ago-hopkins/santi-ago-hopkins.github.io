0b51f8a33b329e026dd6f1af7e33fb3cc8509add
santiagohopkins <santiagoahopkins@gmail.com>
2025-01-19 16:03:46 -0800

    updated headline

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,9 +30,9 @@ export default function Home() {
           onComplete={() => setTitleComplete(true)}
         />
         <FlippingText 
-          text="a mechanical engineer and programmer making a difference in ________???"
+          text="a mechanical engineer and programmer making a difference in robotics"
           delay={titleComplete ? 0 : 1500}
-          highlightWords={['mechanical engineer', 'programmer', '________???']}
+          highlightWords={['mechanical engineer', 'programmer', 'robotics']}
           style={{
             fontSize: '1.5rem',
             color: '#333',
