e537696a1611e1e52b21fafec47bd0bb1cbdf99f
santiagohopkins <santiagoahopkins@gmail.com>
2025-01-25 10:36:55 -0800

    updated resume, again

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -56,7 +56,7 @@ const Navigation = () => {
       <NavLink to="/contact">Contact</NavLink>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/projects">Projects</NavLink>
-      <NavLink to="/pdfs/tl-santiago_hopkins_resume.pdf" isPdf>Resume</NavLink>
+      <NavLink to="/pdfs/r-santiago_hopkins_resume.pdf" isPdf>Resume</NavLink>
       <NavLink to="/pdfs/santiago-hopkins_portfolio_jan2025.pdf" isPdf>Portfolio</NavLink>
       <NavLink to="/pdfs/dbw-1.pdf" isPdf>Drive-by-Wire Project Docs</NavLink>
       <NavLink to="/fun">For Fun</NavLink>
